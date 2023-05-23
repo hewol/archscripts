dfde6457ef2a6d92d22fef47ef29c0922533f04a
Debarka Kundu <90315363+Debarka-Kundu@users.noreply.github.com>
2023-05-23 11:31:21 +0530

    Delete desktop directory

--- desktop/installall.sh
+++
@@ -1,5 +0,0 @@
-echo "Looks like you are crazy but alright we guess..."
-echo "GNOME, KDE and Xfce4 minimal's will be installed with gdm default"
-sudo pacman -Syyy gnome plasma plasma-wayland-session xfce4 gdm sddm lightdm --noconfirm
-sudo systemctl enable gdm
-echo "Reboot needed"
--- desktop/minimal/gnome.sh
+++
@@ -1,6 +0,0 @@
-echo "GNOME minimal will be now installed!"
-sudo pacman -Syyy gdm --noconfirm
-sudo systemctl enable gdm
-sudo pacman -Syyy gnome gdm --noconfirm
-sudo systemctl enable gdm
-echo "Reboot needed"
